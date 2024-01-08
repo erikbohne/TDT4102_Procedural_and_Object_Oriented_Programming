a7a7c9fb4aab0636749c2b6202ae9cb6e60e1549
Erik Bohne <eriknbohne@gmail.com>
2024-01-08 11:40:22 +0100

    blank project

---
+++ pset0/main.cpp
@@ -0,0 +1,21 @@
+//
+// This is example code from Chapter 2.2 "The classic first program" of
+// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
+//
+// This program outputs the message "Hello, World!" to the monitor
+
+#include "std_lib_facilities.h"
+
+//------------------------------------------------------------------------------'
+
+// C++ programs start by executing the function main
+int main() {
+    // Show a nice message in the Terminal window
+    cout << "Hello, World!" << endl;
+
+    // This lets the operating system (Windows, Mac, Linux, etc.) know that the program
+    // did not encounter any errors
+    return 0;
+}
+
+//------------------------------------------------------------------------------
