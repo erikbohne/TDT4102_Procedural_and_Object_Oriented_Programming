d962ad04911448f831fa12a23b517021736a928e
Erik Bohne <eriknbohne@gmail.com>
2024-01-12 08:21:38 +0100

    pset0

--- pset0/main.cpp
+++ pset0/main.cpp
@@ -1,7 +1,21 @@
-// Simple hello world program
+//
+// This is example code from Chapter 2.2 "The classic first program" of
+// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
+//
+// This program outputs the message "Hello, World!" to the monitor
+
 #include "std_lib_facilities.h"
 
+//------------------------------------------------------------------------------'
+
+// C++ programs start by executing the function main
 int main() {
-    cout << "Hello, world!" << endl;
+    // Show a nice message in the Terminal window
+    cout << "Hello, World!" << endl;
+
+    // This lets the operating system (Windows, Mac, Linux, etc.) know that the program
+    // did not encounter any errors
     return 0;
 }
+
+//------------------------------------------------------------------------------
