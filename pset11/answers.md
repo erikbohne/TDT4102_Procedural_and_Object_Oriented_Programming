0b2bd2bbc2700c23b8493a6443784044f06d8cb3
erikbohne <eriknbohne@gmail.com>
2024-04-10 13:37:50 +0200

    task 2

--- pset11/answers.md
+++ pset11/answers.md
@@ -14,3 +14,28 @@ delete arr2;
 ```
 
 Time taken on 1.000.000.000 iterations: 20.2793 seconds
+
+
+# Task 2
+
+> a) Hva skjer når vektorens lengde overskrider kapasiteten til arrayet?
+
+When the length of the vector surpasses the assigned array, it allocates more memory by doubeling the capacity. 
+
+> b) Hva skjer nå med size() og capacity()?
+
+After reserving the vector has a capacity of 20, we do not need to reallocate whenever the vector gets full. 
+
+> c) Hva skjer hvis vi istedenfor benytter medlemsfunksjonen resize(20)? 
+
+If we use the resize function, the vector will be resized to 20 elements. If the vector is smaller than 20, it will be filled with 0. If the vector is larger than 20, the elements will be truncated.
+
+
+> d) Hvor lang tid tar det å pushe 1.000.000 elementer til en vector?
+
+0.027541 seconds
+
+> e) Hva om vi bruker resize(1.000.000) og reserve(1.000.000) før vi pusher elementene?
+
+resize : 0.035726 seconds
+reserve: 0.013925 seconds
--- pset11/main.cpp
+++ pset11/main.cpp
@@ -1,10 +1,26 @@
 #include "Stopwatch.h"
 #include <array>
+#include <vector>
+
+void timeTask1();
+void timeTask2();
 
 constexpr int size = 10000;
 constexpr int N = 1000000000;
 
 int main() {
+    
+    // Task 1
+    timeTask1();
+
+    // Task 2
+    timeTask2();
+
+    return 0;
+}
+
+
+void timeTask1 () {
     Stopwatch stopwatch;
     stopwatch.start();
 
@@ -23,5 +39,51 @@ int main() {
 
     double time = stopwatch.stop();
     std::cout << "Time taken: " << time << " seconds" << std::endl;
-    return 0;
+}
+
+void timeTask2 () {
+    Stopwatch stopwatch;
+    stopwatch.start();
+
+    // ----- START OF CODE TO TIME ----- //
+
+
+    // Task a
+    // std::vector<int> vec;
+    // for (int i = 0; i < 20; ++i) {
+    //     vec.push_back(i);
+    //     std::cout << "Size: " << vec.size() << ", Capacity: " << vec.capacity() << std::endl;
+    // }
+
+    // Task b
+    // std::vector<int> vec;
+    // vec.reserve(20); // Reserver plass
+    // for (int i = 0; i < 20; ++i) {
+    //     vec.push_back(i);
+    //     std::cout << "Size: " << vec.size() << ", Capacity: " << vec.capacity() << std::endl;
+    // }
+
+    // Task c
+    // std::vector<int> vec;
+    // vec.resize(20);
+    // std::cout << "Initial Size: " << vec.size() << ", Initial Capacity: " << vec.capacity() << std::endl;
+
+    // Task d
+    // std::vector<int> vec;
+    // for (int i = 0; i < 1000000; ++i) {
+    //     vec.push_back(i);
+    // }
+
+    // Task e
+    std::vector<int> vec;
+    // vec.reserve(1000000);
+    vec.resize(1000000);
+    for (int i = 0; i < 1000000; ++i) {
+        vec.push_back(i);
+    }
+
+    // ----- END OF CODE TO TIME ----- //
+
+    double time = stopwatch.stop();
+    std::cout << "Time taken: " << time << " seconds" << std::endl;
 }
